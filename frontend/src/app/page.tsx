514b971fb59ce988f7a9094ee9af61d44808b06f
yasasbanukaofficial <yasasbanukaoffical@gmail.com>
2025-06-18 00:29:58 +0530

    feat(button): added a speak to agent btn

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,17 @@
 import ParticlesBackground from "@/components/ui/ParticlesBackground";
 import SplitText from "@/components/ui/SplitText";
 import RotatingText from "@/components/ui/RotatingText";
+import Button from "@/components/ui/Button";
 
 export default function Home() {
   return (
     <>
       <div className="fixed inset-0 -z-10 w-screen h-screen overflow-hidden">
         <ParticlesBackground
-          particleColors={['#235aa9', '#ffffff']}
-          particleCount={200}
+          particleColors={['#235aa9', '#ffffff', '#790000']}
+          particleCount={400}
           particleSpread={10}
-          speed={0.1}
+          speed={0.2}
           particleBaseSize={120}
           moveParticlesOnHover={true}
           alphaParticles={false}
@@ -18,33 +19,39 @@ export default function Home() {
         />
       </div>
 
-      <div className="fixed inset-0 flex items-center justify-center gap-12 -translate-y-12 overflow-hidden z-10 px-4 max-w-full w-full">
+      <div className="fixed inset-0 flex flex-col items-center justify-center gap-6 -translate-y-12 overflow-hidden z-10 px-4 max-w-full w-full">
+          <div className="flex flex-row items-center justify-center gap-12">
+            <div className="flex-shrink-0 flex items-center justify-center">
+              <SplitText
+                text="Travel with"
+                className="
+                  font-bold text-center
+                  text-[clamp(1rem,14vw,4rem)]
+                  md:[font-size:clamp(4rem,10vw,8rem)]
+                  leading-tight
+                "
+              />
+            </div>
 
-        <div className="flex-shrink-0 flex items-center justify-center">
-          <SplitText
-            text="Travel with"
-            className="
-              font-bold text-center while
-              text-[clamp(1rem,14vw,4rem)]
-              md:[font-size:clamp(4rem,10vw,8rem)]
-              leading-tight
-            "
-          />
+            <div className="flex-shrink-0 flex items-center justify-center">
+              <RotatingText
+                texts={["Ease", "Style", "Confidence", "Guide", "Travelop"]}
+                className="
+                  font-extrabold
+                  text-[clamp(2rem,18vw,8rem)]
+                  md:[font-size:clamp(3rem,8vw,8rem)]
+                  leading-none
+                  px-2 sm:px-2 md:px-3 bg-white text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg
+                "
+              />
+            </div>
+          </div>
+
+          <div className={"mt-9"}>
+            <Button />
+          </div>
         </div>
 
-        <div className="flex-shrink-0 flex items-center justify-center">
-            <RotatingText
-            texts={["Ease", "Style", "Confidence", "Guide", "Travelop"]}
-            className="
-              font-extrabold
-              text-[clamp(2rem,18vw,8rem)]
-              md:[font-size:clamp(3rem,8vw,8rem)]
-              leading-none
-              px-2 sm:px-2 md:px-3 bg-white text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg
-            "
-          />
-        </div>
-      </div>
     </>
   );
 }
---
+++ frontend/src/components/ui/Button.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export function Button() {
+    return (
+        <button className="px-6 py-3 text-lg font-bold text-black bg-white border border-black rounded rounded-xl hover:bg-black hover:text-white transition-all duration-300 ease-in-out cursor-pointer">
+            Speak to our agent
+        </button>
+    )
+}
+export default Button;
